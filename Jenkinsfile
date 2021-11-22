823e6de7f4456c7063cc16de3440eecb71e84532
Zakaria Guennoune <zguennoune@centreon.com>
2021-11-22 12:31:58 +0000

    packaging as400

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,2 +1,11 @@
 pipeline {
+  agent { label 'aws' } 
+  stages {
+    stage('Packaging AS400') {
+      steps {
+        echo "Packaging AS400"
+        sh 'ci/as400-packaging.sh'
+      }
+    }
+  }
 }
---
+++ ci/as400-packaging.sh
@@ -0,0 +1,16 @@
+!#/bin/bash
+
+set -e
+
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION / RELEASE variables"
+  exit 1
+fi
+
+if [ ! -d /root/rpmbuild/SOURCES ] ; then
+  mkdir -p /root/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+fi
+
+tar czf centreon-plugin-Operatingsystems-AS400-daemon-2.0.0.tar.gz centreon-as400 
+mv centreon-plugin-Operatingsystems-AS400-daemon-2.0.0.tar.gz /root/rpmbuild/SOURCES/
+rpmbuild -ba rpm/centreon-plugin-Operatingsystems-AS400-daemon.spectemplate -D "VERSION $VERSION" -D "RELEASE $RELEASE"
