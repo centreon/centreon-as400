83c9ced98921f4de861f285ea71b9ad7e30d4bb2
Zakaria Guennoune <zguennoune@centreon.com>
2021-11-22 13:11:37 +0000

    testing

--- ci/as400-packaging.sh
+++ ci/as400-packaging.sh
@@ -9,7 +9,6 @@ if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
   echo "You need to specify VERSION / RELEASE variables"
   exit 1
 fi
-sudo apt-get install maven
 sudo rm -rf /$HOME/rpmbuild
 
 if [ ! -d /$HOME/rpmbuild/SOURCES ] ; then
@@ -18,4 +17,4 @@ fi
 
 sudo tar czf centreon-plugin-Operatingsystems-AS400-daemon-2.0.0.tar.gz connector.as400 connector.as400.install doc rpm changelog LICENSE
 sudo mv centreon-plugin-Operatingsystems-AS400-daemon-2.0.0.tar.gz /$HOME/rpmbuild/SOURCES
-rpmbuild -ba rpm/centreon-plugin-Operatingsystems-AS400-daemon.spectemplate -D "VERSION $VERSION" -D "RELEASE $RELEASE"
+sudo rpmbuild -ba rpm/centreon-plugin-Operatingsystems-AS400-daemon.spectemplate -D "VERSION $VERSION" -D "RELEASE $RELEASE"
