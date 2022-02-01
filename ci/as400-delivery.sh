629b3042a562d6bf46baeb197ec793e734445d1b
schapron <schapron@centreon.com>
2022-02-01 17:57:27 +0100

    remove el8 on ci

--- ci/as400-delivery.sh
+++ ci/as400-delivery.sh
@@ -7,21 +7,21 @@ REPO=$1
 VERSION=`grep '<version>.*</version>' connector.as400/pom.xml | cut -d '>' -f 2 | cut -d '<' -f 1 | head -n1`
 
 AS400RPMSEL7=`echo noarch/*.el7.*.rpm`
-AS400RPMSEL8=`echo noarch/*.el8.*.rpm`
+#AS400RPMSEL8=`echo noarch/*.el8.*.rpm`
 
 # Publish RPMs
 if [ "$1" = "testing" ] ; then
   put_rpms "standard" "20.10" "el7" "$REPO" "noarch" "centreon-as400" "centreon-as400-$VERSION-$BUILD_NUMBER" $AS400RPMSEL7
-  put_rpms "standard" "20.10" "el8" "$REPO" "noarch" "centreon-as400" "centreon-as400-$VERSION-$BUILD_NUMBER" $AS400RPMSEL8
+#  put_rpms "standard" "20.10" "el8" "$REPO" "noarch" "centreon-as400" "centreon-as400-$VERSION-$BUILD_NUMBER" $AS400RPMSEL8
   put_rpms "standard" "21.04" "el7" "$REPO" "noarch" "centreon-as400" "centreon-as400-$VERSION-$BUILD_NUMBER" $AS400RPMSEL7
-  put_rpms "standard" "21.04" "el8" "$REPO" "noarch" "centreon-as400" "centreon-as400-$VERSION-$BUILD_NUMBER" $AS400RPMSEL8
+#  put_rpms "standard" "21.04" "el8" "$REPO" "noarch" "centreon-as400" "centreon-as400-$VERSION-$BUILD_NUMBER" $AS400RPMSEL8
   put_rpms "standard" "21.10" "el7" "$REPO" "noarch" "centreon-as400" "centreon-as400-$VERSION-$BUILD_NUMBER" $AS400RPMSEL7
-  put_rpms "standard" "21.10" "el8" "$REPO" "noarch" "centreon-as400" "centreon-as400-$VERSION-$BUILD_NUMBER" $AS400RPMSEL8
+#  put_rpms "standard" "21.10" "el8" "$REPO" "noarch" "centreon-as400" "centreon-as400-$VERSION-$BUILD_NUMBER" $AS400RPMSEL8
 else
   put_rpms "standard" "20.10" "el7" "$REPO" "noarch" "RPMS" "" $AS400RPMSEL7
-  put_rpms "standard" "20.10" "el8" "$REPO" "noarch" "RPMS" "" $AS400RPMSEL8
+#  put_rpms "standard" "20.10" "el8" "$REPO" "noarch" "RPMS" "" $AS400RPMSEL8
   put_rpms "standard" "21.04" "el7" "$REPO" "noarch" "RPMS" "" $AS400RPMSEL7
-  put_rpms "standard" "21.04" "el8" "$REPO" "noarch" "RPMS" "" $AS400RPMSEL8
+#  put_rpms "standard" "21.04" "el8" "$REPO" "noarch" "RPMS" "" $AS400RPMSEL8
   put_rpms "standard" "21.10" "el7" "$REPO" "noarch" "RPMS" "" $AS400RPMSEL7
-  put_rpms "standard" "21.10" "el8" "$REPO" "noarch" "RPMS" "" $AS400RPMSEL8
+#  put_rpms "standard" "21.10" "el8" "$REPO" "noarch" "RPMS" "" $AS400RPMSEL8
 fi
